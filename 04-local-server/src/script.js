157f160c7b0da1468af3c8c43eaa1368953915ad
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-06-20 16:58:59 -0400

    gsap dependencies added

--- 04-local-server/src/script.js
+++ 04-local-server/src/script.js
@@ -1,17 +1,47 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
+console.log(gsap)
 // Scene
 const scene = new THREE.Scene()
+
+// Making a group
+// const group = new THREE.Group()
+// scene.add(group)
+
+// const cube1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({color: 'blue'})
+// )
+
+// const cube2 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({color: 'green'})
+// )
+
+// cube2.position.x = -2
+// group.add(cube1)
+// group.add(cube2)
+
+// group.position.y = 1
+
 // Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 'red'})
+const material = new THREE.MeshBasicMaterial({color: 'orange'})
 const mesh = new THREE.Mesh(geometry, material)
 
-mesh.position.x = 1
-mesh.rotation.y = 2
-mesh.rotation.z = -3
+// Cube transformations
+// mesh.position.x = 1
+// mesh.rotation.y = 2
+// mesh.rotation.z = -3
+
+// mesh.scale.y = .5
 
 scene.add(mesh)
+
+// Axis helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 //sizes
 const sizes = {
@@ -19,8 +49,10 @@ const sizes = {
     height: 600
 }
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
+
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -31,3 +63,27 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 renderer.render(scene, camera)
+
+// Animation
+
+// gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+
+let time = Date.now()
+
+const tick = () => {
+    // fixing frame rate issues
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+
+    // update objects
+    // mesh.rotation.y += 0.001 * deltaTime
+
+    // Renderer
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+
+}
+tick()
