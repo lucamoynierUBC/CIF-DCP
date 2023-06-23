7b44d6f6c4f5af26eca3636178905d47e2d621e4
lucamoynierUBC <lmoynier@student.ubc.ca>
2023-06-23 12:59:10 -0400

    forgot to stage

--- project/src/Experience/sources.js
+++ project/src/Experience/sources.js
@@ -1,5 +1,8 @@
 export default [
     {
+        //name: used to retrieve the loaded resource
+        //type: in order to know what loader to use
+        //path: the path of the file to load
         name: 'environmentMapTexture',
         type: 'cubeTexture',
         path:
